ececea5575a510cc42868a6115a3022caf994569
leemch <lee7mchale@gmail.com>
2018-12-29 14:35:15 -0800

    prepared for deply

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-	mongoURI: "mongodb://leemch:katagiri1@ds155288.mlab.com:55288/trainerconnector",
-	secretOrKey: "secret"
-
+if(process.env.NODE_ENV === "production") {
+	module.exports = require("./keys_prod");
+} else {
+	module.exports = require("./keys_dev");
 }
