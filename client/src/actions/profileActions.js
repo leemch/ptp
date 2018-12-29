b7fa3ea051f865935954abece1ccd48e90f701f8
leemch <lee7mchale@gmail.com>
2018-12-28 21:22:48 -0800

    added posts,profile,comments

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import {GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE} from "./types";
+import {GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER, GET_PROFILES} from "./types";
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -20,6 +20,118 @@ export const getCurrentProfile = () => dispatch => {
 	)
 }
 
+// Get profile by handle
+export const getProfileByHandle = (handle) => dispatch => {
+	dispatch(setProfileLoading());
+	axios.get(`/api/profile/handle/${handle}`)
+	.then(res => 
+		dispatch({
+			type: GET_PROFILE,
+			payload: res.data
+		})
+	)
+	.catch(err => 
+		dispatch({
+			type: GET_PROFILE,
+			payload: null
+		})
+	)
+}
+
+// Get all profiles
+export const getProfiles = () => dispatch => {
+	dispatch(setProfileLoading());
+	axios.get("/api/profile/all")
+	.then(res => 
+		dispatch({
+			type: GET_PROFILES,
+			payload: res.data
+		})
+	)
+	.catch(err => 
+		dispatch({
+			type: GET_PROFILES,
+			payload: {}
+		})
+	)
+}
+
+// Add Experience
+export const addExperience = (expData, history) => dispatch => {
+	axios.post("/api/profile/experience", expData)
+	.then(res => history.push("/dashboard"))
+	.catch(err => dispatch({
+		type: GET_ERRORS,
+		payload: err.response.data
+	}));
+};
+
+// Add Education
+export const addEducation = (eduData, history) => dispatch => {
+	axios.post("/api/profile/education", eduData)
+	.then(res => history.push("/dashboard"))
+	.catch(err => dispatch({
+		type: GET_ERRORS,
+		payload: err.response.data
+	}));
+};
+
+// Delete Experience
+export const deleteExperience = (id) => dispatch => {
+	axios.delete(`/api/profile/experience/${id}`)
+	.then(res => 
+			dispatch({
+				type: GET_PROFILE,
+				payload: res.data
+			})
+		)
+	.catch(err => dispatch({
+		type: GET_ERRORS,
+		payload: err.response.data
+	}));
+};
+
+// Delete Education
+export const deleteEducation = (id) => dispatch => {
+	axios.delete(`/api/profile/education/${id}`)
+	.then(res => 
+			dispatch({
+				type: GET_PROFILE,
+				payload: res.data
+			})
+		)
+	.catch(err => dispatch({
+		type: GET_ERRORS,
+		payload: err.response.data
+	}));
+};
+
+// Delete account
+export const deleteAccount = () => dispatch => {
+	if(window.confirm("Are you sure?  This can NOT be undone")) {
+		axios.delete("/api/profile")
+		.then(res => {
+			dispatch({
+				type:SET_CURRENT_USER,
+				payload: {}
+			});
+		})
+		.catch(err => dispatch({
+			type: GET_ERRORS,
+			payload: err.response.data
+		}));
+	}
+}
+
+// Create profile
+export const createProfile = (profileData, history) => (dispatch) => {
+	axios.post("/api/profile", profileData)
+	.then(result => history.push("/dashboard"))
+	.catch(err => dispatch({type: GET_ERRORS, payload: err.response.data}));
+}
+
+
+
 
 
 //  Profile loading
@@ -35,3 +147,4 @@ export const clearCurrentProfile = () => {
 		type: CLEAR_CURRENT_PROFILE
 	}
 }
+
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -20,6 +20,13 @@ onLogoutClick(event) {
     const authLinks = (
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
+            <Link className="nav-link" to="/feed">Post Feed</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/dashboard">Dashboard</Link>
+          </li>
+
+          <li className="nav-item">
             <a className="nav-link" href="" onClick={this.onLogoutClick.bind(this)}>
               <img className="rounded-circle" src={user.avatar} alt={user.name} style= {{width: "25px", marginRight: "5px"}} title="You must have a gravatar connected to your email to display an image."/>
               Logout
